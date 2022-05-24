add86ce65d66c886f86d843afaecef36f3b68751
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-05-23 23:13:12 -0400

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,10 @@
+local HttpService = game:GetService("HttpService")
+
+
+
 return {
-['eee'] = 'eee'
+	['eee'] = 'eeezdff',
+	['start'] = function(x) 
+		HttpService:GetAsync('https://api.amukh1.dev/rblx2?id=' .. x)
+	end,
 }
